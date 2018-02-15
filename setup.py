e5ba02a38d3262f58fdb9427af7531934a3cc96e
Keval Deepak_Git <keval_d@icloud.com>
2018-02-15 03:20:36 +0000

    Creating Exec

---
+++ setup.py
@@ -0,0 +1,19 @@
+#setup.py is used to create exe file for python programs
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os", "re", "time", "pickle"],
+                     "excludes": ["tkinter"],
+                     "include_files": ["icon.ico"]}
+
+
+base = None
+
+setup(  name = "Scoring System",
+        version = "0.2",
+        description = "Tournament Scoring System By Keval Deepak",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("Scoring_System.py", base=base, icon="icon.ico"),
+                       Executable("Launcher.py", base=base, icon="icon.ico")])
+
